dc1b99e79a3f463579214e65fc41a2995927479a
GenerativeGig <generativegig@gmail.com>
2023-05-31 18:48:38 +0200

    Create new  package with vite

---
+++ web-old/src/App.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import banner from "./g-banner-transparent.png";
+import logo from "./logo64.png";
+import livestream from "./g-livestream.png";
+import instagram from "./g-instagram.png";
+import twitter from "./g-twitter.png";
+
+function App() {
+  return (
+    <div id="root">
+      <header>
+        <img alt="logo" src={banner} />
+      </header>
+      <main>
+        <p>
+          <strong>Here comes the music of the future!</strong>
+        </p>
+        <p id="inner-dialog">
+          <i>Uhm, that should be convincing enough... Perfect!</i>
+        </p>
+        <p>Hang on a minute, you actually believed that? HAHA! </p>
+        <p>
+          Anyways, you should check out our music. We do like a live techno esq
+          thing on some crazy cool equipment. <br></br>Who knows, it might be
+          your kind of jam...
+        </p>
+        <ul>
+          <li>
+            <a
+              href="https://www.youtube.com/channel/UC7nKgwyOW6FkC3HxEnxHxJg"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img alt="livestream" src={livestream} />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/generativegig/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img alt="instagram" src={instagram} />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://twitter.com/generativegig"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img alt="twitter" src={twitter} />
+            </a>
+          </li>
+        </ul>
+      </main>
+      <footer>
+        <img alt="logo" src={logo} />
+      </footer>
+    </div>
+  );
+}
+
+export default App;
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,64 +1,34 @@
-import React from "react";
-import banner from "./g-banner-transparent.png";
-import logo from "./logo64.png";
-import livestream from "./g-livestream.png";
-import instagram from "./g-instagram.png";
-import twitter from "./g-twitter.png";
+import { useState } from 'react'
+import './App.css'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
-    <div id="root">
-      <header>
-        <img alt="logo" src={banner} />
-      </header>
-      <main>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
         <p>
-          <strong>Here comes the music of the future!</strong>
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-        <p id="inner-dialog">
-          <i>Uhm, that should be convincing enough... Perfect!</i>
-        </p>
-        <p>Hang on a minute, you actually believed that? HAHA! </p>
-        <p>
-          Anyways, you should check out our music. We do like a live techno esq
-          thing on some crazy cool equipment. <br></br>Who knows, it might be
-          your kind of jam...
-        </p>
-        <ul>
-          <li>
-            <a
-              href="https://www.youtube.com/channel/UC7nKgwyOW6FkC3HxEnxHxJg"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img alt="livestream" src={livestream} />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.instagram.com/generativegig/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img alt="instagram" src={instagram} />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://twitter.com/generativegig"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img alt="twitter" src={twitter} />
-            </a>
-          </li>
-        </ul>
-      </main>
-      <footer>
-        <img alt="logo" src={logo} />
-      </footer>
-    </div>
-  );
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
