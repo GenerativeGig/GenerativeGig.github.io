3a42b3287be340651e1e2173eb797688a87373e3
GenerativeGig <generativegig@gmail.com>
2024-05-29 00:57:19 +0200

    Fix some bugs

--- web/main.js
+++ web/main.js
@@ -1,4 +1,4 @@
-let currentContentBlock = 'philosophy';
+let currentContentBlock = { id: 'philosophy', title: 'Philosophy' };
 
 const navListElements = document.getElementsByClassName('content-block-link');
 const navListElementsArray = Array.from(navListElements);
@@ -8,23 +8,30 @@ navListElementsArray.forEach((element) => {
 });
 
 function handleContentBlockLinkClick(event) {
-  setContentBlock(event.target.getAttribute('data-content-block'));
+  setContentBlock({
+    id: event.target.getAttribute('data-content-block'),
+    title:
+      event.target.innerText === 'GenerativeGig'
+        ? 'Philosophy'
+        : event.target.innerText,
+  });
 }
 
 function setContentBlock(newContentBlock) {
-  const previousContentBlockElement =
-    document.getElementById(currentContentBlock);
+  const previousContentBlockElement = document.getElementById(
+    currentContentBlock.id
+  );
   previousContentBlockElement.style.display = 'none';
 
   const previousContentBlockLinkElement = document.getElementById(
-    `${currentContentBlock}-link`
+    `${currentContentBlock.id}-link`
   );
   previousContentBlockLinkElement.style.textDecoration = 'none';
 
   if (
-    currentContentBlock === 'generativegig' ||
-    currentContentBlock === 'alien-paul-x-generativegig' ||
-    currentContentBlock === 'portal-k09'
+    currentContentBlock.id === 'generativegig' ||
+    currentContentBlock.id === 'alien-paul-x-generativegig' ||
+    currentContentBlock.id === 'portal-k09'
   ) {
     const liveActsNavButtonElement = document.getElementById('live-acts');
     liveActsNavButtonElement.style.textDecoration = 'none';
@@ -32,24 +39,47 @@ function setContentBlock(newContentBlock) {
 
   currentContentBlock = newContentBlock;
 
-  const currentContentBlockElement =
-    document.getElementById(currentContentBlock);
+  const currentContentBlockElement = document.getElementById(
+    currentContentBlock.id
+  );
   currentContentBlockElement.style.display = 'block';
 
   const currentContentBlockLinkElement = document.getElementById(
-    `${currentContentBlock}-link`
+    `${currentContentBlock.id}-link`
   );
   currentContentBlockLinkElement.style.textDecoration = 'overline';
 
   if (
-    currentContentBlock === 'generativegig' ||
-    currentContentBlock === 'alien-paul-x-generativegig' ||
-    currentContentBlock === 'portal-k09'
+    currentContentBlock.id === 'generativegig' ||
+    currentContentBlock.id === 'alien-paul-x-generativegig' ||
+    currentContentBlock.id === 'portal-k09'
   ) {
     const liveActsNavButtonElement = document.getElementById('live-acts');
     liveActsNavButtonElement.style.textDecoration = 'overline';
   }
+
+  if (window.innerWidth <= 800) {
+    const headerContent = document.querySelector('.header-content');
+
+    headerContent.style.display = 'none';
+
+    const titleElement = document.getElementById('page-title');
+
+    titleElement.innerText = currentContentBlock.title;
+  }
+
+  const liveActsMenu = document.getElementById('nav-ul-ul-live-acts');
+
+  liveActsMenu.style.display = 'none';
 }
+
+window.addEventListener('load', () => {
+  if (window.innerWidth <= 800) {
+    const titleElement = document.getElementById('page-title');
+
+    titleElement.innerText = currentContentBlock.title;
+  }
+});
 
 const hamburger = document.querySelector('.hamburger');
 const headerContent = document.querySelector('.header-content');
@@ -69,7 +99,24 @@ window.addEventListener('click', (event) => {
 });
 
 window.addEventListener('resize', function () {
+  const titleElement = document.getElementById('page-title');
+
   if (window.innerWidth >= 801) {
     headerContent.style.display = 'flex';
+    titleElement.innerText = '';
+  } else {
+    titleElement.innerText = currentContentBlock.title;
+  }
+});
+
+const liveActsNavButtonElement = document.getElementById('live-acts');
+
+liveActsNavButtonElement.addEventListener('click', () => {
+  const liveActsMenu = document.getElementById('nav-ul-ul-live-acts');
+
+  if (liveActsMenu.style.display === 'none') {
+    liveActsMenu.style.display = 'block';
+  } else {
+    liveActsMenu.style.display = 'none';
   }
 });
